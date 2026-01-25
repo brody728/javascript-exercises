5bffd8997caf026c2ae86ce4a761ef55e4274c86
Brody Livingston <57334476+brody728@users.noreply.github.com>
2026-01-24 22:53:23 -0700

    Write solution for foundations/07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let outputStr = '';
+    for (let i = 0; i < str.length; i++) {
+        outputStr = str.charAt(i) + outputStr;
+    }
 
+    return outputStr;
 };
 
 // Do not edit below this line
