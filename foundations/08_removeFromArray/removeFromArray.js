aeaa5c2556f8a245a6b7ab66e5bf65c4c9c4a6a6
Brody Livingston <57334476+brody728@users.noreply.github.com>
2026-01-24 23:13:49 -0700

    Write solution for foundations/08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const argArray = Array.from(arguments);
+    const itemsToRemove = argArray.slice(1);
+
+    return arr.filter((item) => !(argArray.includes(item)));
 };
 
 // Do not edit below this line
