9feb6414fd9d4f756676355ddd78a06b7cfeb58e
Brody Livingston <57334476+brody728@users.noreply.github.com>
2026-01-24 23:27:19 -0700

    Write solutions for foundations/09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0) return 'ERROR';
+    if (!(Number.isInteger(x) && Number.isInteger(y))) return 'ERROR';
+    let sum = 0;
+    for (let i = Math.min(x, y); i <= Math.max(x, y); i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
