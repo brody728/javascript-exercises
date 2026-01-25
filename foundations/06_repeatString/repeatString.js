5765107a2e52dc7958b5ba00c5c5d771434a065d
Brody Livingston <57334476+brody728@users.noreply.github.com>
2026-01-24 22:48:20 -0700

    Write solution for foundations/06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, repititions) {
+    if (repititions < 0) return 'ERROR';
+    
+    let outputStr = ''
+    for (let i = 0; i < repititions; i++) {
+        outputStr += str;
+    }
+    return outputStr;
 };
 
 // Do not edit below this line
