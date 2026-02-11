8fac62aac94c8ee6640d601bfcd74dd62b2e0baf
Brody Livingston <57334476+brody728@users.noreply.github.com>
2026-02-10 17:17:46 -0700

    Solve 12 and 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (x) {
+    const regexp = /[a-z1-9]{1}/g
+    let onlyLetters = Array.from(x.toLowerCase().matchAll(regexp), m => m[0]);
 
+    console.log(onlyLetters);
+   
+    for (let i = 0; i < onlyLetters.length / 2; i++) {
+        if (onlyLetters[i] !== onlyLetters[onlyLetters.length - i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
