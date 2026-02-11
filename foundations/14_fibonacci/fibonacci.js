81cd91ffb3a513c6fd400a096ccd58a146d7c238
Brody Livingston <57334476+brody728@users.noreply.github.com>
2026-02-10 17:29:22 -0700

    Solve 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS";
+    if (x == 0) return 0;
+    let prev = 0;
+    let current = 1;
+    for (let i = 1; i < + x; i++) {
+        console.log(current);
+        const tmp = current;
+        current += prev;
+        prev = tmp;
+    }
 
+    return current;
 };
 
 // Do not edit below this line
