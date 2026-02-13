db9ab48641ec7de3e3a439662c4143160424c345
Brody Livingston <57334476+brody728@users.noreply.github.com>
2026-02-13 15:26:22 -0700

    Add button tag to button divs

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	let sumValue = 0;
+  sumArray.forEach((item) => sumValue += item);
+  return sumValue;
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArray) {
+  let product = 1;
+  multiplyArray.forEach((item) => product *= item);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let product = 1;
+	for (let i = 1; i <= x; i++) product *= i;
+  return product;
 };
 
 // Do not edit below this line
