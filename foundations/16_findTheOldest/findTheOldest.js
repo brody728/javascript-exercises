a50ff1282e485db3c6b62ab700b91a215a72560d
Brody Livingston <57334476+brody728@users.noreply.github.com>
2026-02-12 18:49:14 -0700

    Write solution for 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    const CURRENTYEAR = 2026;
+    let oldestPerson;
+    let oldestPersonAge = 0;
+    for (person of peopleArr) {
+        let age;
+        if (!person.yearOfDeath) {
+            age = CURRENTYEAR - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
 
+        if (age > oldestPersonAge) {
+            oldestPerson = person;
+            oldestPersonAge = age;
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
